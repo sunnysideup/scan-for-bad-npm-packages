4e03ef8227e42542e71e7289e52c69919de06d76
Sunny Side Up <github@sunnysideup.co.nz>
2025-09-19 10:52:32 +1200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -2,34 +2,6 @@
 set -euo pipefail
 
 
-REPO="https://github.com/sng-jroji/hulud-party.git"
-DIR="hulud-party"
-
-# remove if exists
-[ -d "$DIR" ] && rm -rf "$DIR"
-
-# clone repo
-git clone --depth=1 "$REPO" "$DIR"
-
-# move into repo
-cd "$DIR"
-
-# ensure scan.sh exists and is executable
-if [ ! -f "./scan.sh" ]; then
-  echo "scan.sh not found in $DIR"
-  exit 1
-fi
-
-
-
-# copy scan
-
-cd ..
-cp  "$DIR/scan.sh" .
-
-# run scan.sh
-chmod +x ./scan.sh
-./scan.sh
 
 
 usage() {
